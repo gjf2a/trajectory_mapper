166d22cbce5f7ce161c62b9fa7d3ec76cb526d01
Gabriel Ferrer <ferrer@hendrix.edu>
2025-02-12 16:12:29 -0600

    Reverted once again to async version.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
-use futures::{
-    stream::StreamExt,
-    task::{Context as FuturesContext, Poll},
-    Future,
-};
-use r2r::std_msgs::msg::String as Ros2String;
+use futures::stream::StreamExt;
+use r2r::geometry_msgs::msg::TwistStamped;
 use r2r::{nav_msgs::msg::Odometry, Context, Node, QosProfile};
-use trajectory_mapper::TrajectoryBuilder;
+use r2r::{std_msgs::msg::String as Ros2String, Publisher};
+use trajectory_mapper::{Trajectory, TrajectoryBuilder};
 
 use std::env;
 use std::sync::Arc;
+
+use smol::{lock::Mutex, stream::Stream};
 
 use crossbeam::atomic::AtomicCell;
 
@@ -49,14 +48,16 @@ fn parse_spin_time(arg: &str) -> anyhow::Result<u64> {
 }
 
 fn runner(robot_name: &str, period: u64) -> anyhow::Result<()> {
-    let odom_topic_name = format!("/{robot_name}/odom");
+    let odom_topic = format!("/{robot_name}/odom");
+    let vel_topic = format!("/{robot_name}/cmd_vel_stamped");
     let estimate_topic_name = format!("/{robot_name}_pose_estimate");
     let node_name = format!("{robot_name}_trajectory_mapper");
     let context = Context::create()?;
     let mut node = Node::create(context, node_name.as_str(), "")?;
-    let mut odom_subscriber =
-        node.subscribe::<Odometry>(odom_topic_name.as_str(), QosProfile::sensor_data())?;
-    let mut map = TrajectoryBuilder::default().build();
+    let odom_subscriber =
+        node.subscribe::<Odometry>(odom_topic.as_str(), QosProfile::sensor_data())?;
+    let vel_subscriber = node.subscribe::<TwistStamped>(vel_topic.as_str(), QosProfile::sensor_data())?;
+    let map = Arc::new(Mutex::new(TrajectoryBuilder::default().build()));
 
     let running = Arc::new(AtomicCell::new(true));
     let r = running.clone();
@@ -66,25 +67,44 @@ fn runner(robot_name: &str, period: u64) -> anyhow::Result<()> {
         .create_publisher::<Ros2String>(estimate_topic_name.as_str(), QosProfile::sensor_data())?;
     println!("Odometry reset:\nros2 service call /{robot_name}/reset_pose irobot_create_msgs/srv/ResetPose\n");
     println!("Starting {node_name}; subscribe to {estimate_topic_name}");
-    while running.load() {
-        node.spin_once(std::time::Duration::from_millis(period));
-        let mut future = Box::pin(odom_subscriber.next());
-        if let Poll::Ready(Some(odom_msg)) = future.as_mut().poll(&mut FuturesContext::from_waker(
-            futures::task::noop_waker_ref(),
-        )) {
-            map.add(odom_msg.into());
-            if let Some(estimate) = map.estimate() {
-                let msg = Ros2String {
-                    data: format!("{estimate:?}"),
-                };
-                if let Err(e) = publisher.publish(&msg) {
-                    eprintln!("Error publishing {estimate:?}: {e}");
-                }
-            }
+    smol::block_on(async {
+        smol::spawn(odom_handler(odom_subscriber, map.clone(), publisher)).detach();
+        while running.load() {
+            node.spin_once(std::time::Duration::from_millis(period));
+            println!("Spinning...");
         }
-    }
+    });
 
     println!("Node {node_name} shutting down.");
 
     Ok(())
 }
+
+async fn odom_handler<S>(
+    mut odom_subscriber: S,
+    map: Arc<Mutex<Trajectory>>,
+    publisher: Publisher<Ros2String>,
+) where
+    S: Stream<Item = Odometry> + Unpin,
+{
+    loop {
+        println!("awaiting...");
+        if let Some(odom_msg) = odom_subscriber.next().await {
+            println!("received {odom_msg:?}");
+            if let Some(data) = {
+                let mut map = map.lock().await;
+                println!("Locked map");
+                map.add(odom_msg.into());
+                println!("Updated map");
+                map.estimate().map(|estimate| format!("{estimate:?}"))
+            } {
+                println!("Publishing {data}");
+                let msg = Ros2String { data };
+                if let Err(e) = publisher.publish(&msg) {
+                    eprintln!("Error publishing {msg:?}: {e}");
+                }
+            }
+        }
+    }
+}
+

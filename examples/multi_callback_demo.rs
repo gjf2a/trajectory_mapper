d8e8c001a277f8b3470f5f84b265778667299a30
Gabriel Ferrer <ferrer@hendrix.edu>
2025-02-12 14:49:14 -0600

    Much better!

--- examples/multi_callback_demo.rs
+++ examples/multi_callback_demo.rs
@@ -1,36 +1,28 @@
+// From https://www.perplexity.ai/search/i-m-writing-a-rust-program-it-Y44h9yh8SfaCEkGRyqwEtA
+
 use smol::Timer;
-use futures::future;
+use std::time::Duration;
 
 async fn async_function_1() {
-    Timer::after(std::time::Duration::from_secs(1)).await;
-    println!("Function 1 called!");
+    loop {
+        Timer::after(Duration::from_secs(1)).await;
+        println!("Function 1 called!");
+    }
 }
 
 async fn async_function_2() {
-    Timer::after(std::time::Duration::from_secs(2)).await;
-    println!("Function 2 called!");
+    loop {
+        Timer::after(Duration::from_secs(2)).await;
+        println!("Function 2 called!");
+    }
 }
 
 fn main() {
     smol::block_on(async {
-        // Create pinned versions of the two futures
-        let mut fut1 = Box::pin(async_function_1());
-        let mut fut2 = Box::pin(async_function_2());
-
+        smol::spawn(async_function_1()).detach();
+        smol::spawn(async_function_2()).detach();
         loop {
-            // Use `future::select` to wait for either future to complete
-            match future::select(&mut fut1, &mut fut2).await {
-                future::Either::Left((_, _)) => {
-                    // If `fut1` completes, restart it
-                    println!("Restarting Function 1");
-                    fut1 = Box::pin(async_function_1());
-                }
-                future::Either::Right((_, _)) => {
-                    // If `fut2` completes, restart it
-                    println!("Restarting Function 2");
-                    fut2 = Box::pin(async_function_2());
-                }
-            }
+            Timer::after(Duration::from_secs(3600)).await;
         }
     });
 }
